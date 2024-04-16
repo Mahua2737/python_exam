69a3766f890c6daf749026b6781dd5a5d1b23b96
Mahua2737 <141414997+Mahua2737@users.noreply.github.com>
2024-04-16 10:24:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import io
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,7 +8,6 @@ from image_processing import process_image
 from text import text_analysis
 
 
-# Load data
 @st.cache
 def load_data():
     data = pd.read_csv("WomensClothingE-CommerceReviews.csv")
@@ -18,7 +19,6 @@ data = load_data()
 menu_options = ["3D plot", "Image processing", "Text Analysis"]
 selected_option = st.sidebar.selectbox("Menu", menu_options)
 
-
 if selected_option == "3D plot":
     st.write("# 3D Plot")
     fig = px.scatter_3d(
@@ -28,11 +28,20 @@ if selected_option == "3D plot":
 
 elif selected_option == "Image processing":
     st.write("# Image Processing")
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    if uploaded_file is not None:
-        image = process_image(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+    images = {
+        "Image 1": "image1.jpg",
+        "Image 2": "image2.jpg",
+        "Image 3": "image3.jpg",
+        "Image 4": "image4.jpg",
+    }
+
+    selected_image = st.selectbox("Choose an image...", list(images.keys()))
+
+    image_path = images[selected_image]
+    image = Image.open(image_path)
+    processed_image = process_image(image)
+    st.image(processed_image, caption="Uploaded Image", use_column_width=True)
 
 elif selected_option == "Text Analysis":
     st.write("# Text Analysis")
